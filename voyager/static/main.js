2cc439b7aaf150bb7ba6e3fc6021957c5dafbd81
Corentin Deleuse <dlse.corentin@gmail.com>
2022-06-26 10:13:25 +0200

    add scroll button

--- voyager/static/main.js
+++ voyager/static/main.js
@@ -1,11 +1,11 @@
-const container = document.getElementById('.elementID'); 
+const container = document.getElementById('.elementID');
 if(window.innerWidth < 999){
     container.innterHTML = '';
     container.innterHTML = `<p>New Text</p>`;
   }
 
 
-/*  
+/*
   JavaScript source code for the video gallery section
   Source : https://codemyui.com/bay-window-style-image-slider-with-scroll-effect/
   Author : Tom Miller
@@ -20,7 +20,7 @@ $(document).ready(function(){gsap.timeline()
     // backgroundImage:(i)=>'url(https://pic.clubic.com/v1/images/1859638/raw)',
     backgroundPosition:(i)=>getBgPos(i),
     backfaceVisibility:'hidden'
-  })    
+  })
   .from('.ring_img', {
     duration:1.5,
     y:200,
@@ -31,8 +31,9 @@ $(document).ready(function(){gsap.timeline()
   .add(()=>{
     $('.ring_img').on('mouseenter', (e)=>{
       let current = e.currentTarget;
-      gsap.to('.ring_img', {opacity:(i,t)=>(t==current)? 1:0.5, ease:'power3'})
-    })
+      gsap.to('.ring_img', {opacity:(i,t)=>(t===current)? 1:0.5, ease:'power3'})
+    });
+
     $('.ring_img').on('mouseleave', (e)=>{
       gsap.to('.ring_img', {opacity:1, ease:'power2.inOut'})
     })
@@ -40,8 +41,8 @@ $(document).ready(function(){gsap.timeline()
 
 $(window).on('mousedown touchstart', dragStart);
 $(window).on('mouseup touchend', dragEnd);
-      
-function dragStart(e){ 
+
+function dragStart(e){
   if (e.touches) e.clientX = e.touches[0].clientX;
   xPos = Math.round(e.clientX);
   gsap.set('.ring', {cursor:'grabbing'})
@@ -50,13 +51,13 @@ function dragStart(e){
 
 
 function drag(e){
-  if (e.touches) e.clientX = e.touches[0].clientX;    
+  if (e.touches) e.clientX = e.touches[0].clientX;
 
   gsap.to('.ring', {
     rotationY: '-=' +( (Math.round(e.clientX)-xPos)%360 ),
     onUpdate:()=>{ gsap.set('.ring_img', { backgroundPosition:(i)=>getBgPos(i) }) }
   });
-  
+
   xPos = Math.round(e.clientX);
 }
 
@@ -71,7 +72,7 @@ function getBgPos(i){ //returns the background-position string to create paralla
   return ( 100-gsap.utils.wrap(0,360,gsap.getProperty('.ring', 'rotationY')-180-i*36)/360*500 )+'px 0px';
 }
 
-/*  
+/*
   JavaScript source code for the "left/right" mouse effect in the video gallery section.
   Tweaked a bit for proper integration.
   Source : https://webdesign.tutsplus.com/tutorials/quick-tip-how-to-manipulate-cursor-appearance-with-css--cms-31825
