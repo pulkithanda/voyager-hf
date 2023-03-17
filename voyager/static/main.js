c6b587c19f72dc260e8c0e8c400a67f1420d7246
Alwin7704 <alwinglifferd7704@gmail.com>
2023-03-17 23:31:01 +0530

    Toggle for Dark Mode were added

--- voyager/static/main.js
+++ voyager/static/main.js
@@ -13,7 +13,28 @@ if(window.innerWidth < 999){
 let loader=document.querySelector('.loader');
 
 window.addEventListener("load",vanish);
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
 
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
 function vanish(){
   loader.classList.add("disppear")
 }
